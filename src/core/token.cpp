d262e6cd69e1e6fd071e6af070d53a4856b39d8a
夏空真宇 <412612365@qq.com>
2026-03-03 20:55:03 +0800

    update

--- src/core/token.cpp
+++ src/core/token.cpp
@@ -1,10 +1,173 @@
 #include "token.hpp"
+#include <array>
+#include <algorithm>
 
 namespace luna::Token
 {
-    std::string to_symbol(Type type)
+    Type toTypeFromKeyword(std::string_view s)
     {
-        switch (type) {
+        static const auto keywords = []()
+        {
+            auto arr = std::to_array<std::pair<std::string_view, Type>>
+            ({
+                {"and", TK_AND},
+                {"or", TK_OR},
+                {"not", TK_NOT},
+
+                {"nil", TK_NIL},
+                {"true", TK_TRUE},
+                {"false", TK_FALSE},
+                {"def", TK_DEF},
+                {"var", TK_VAR},
+                {"let", TK_LET},
+                {"if", TK_IF},
+                {"elif", TK_ELIF},
+                {"else", TK_ELSE},
+                {"loop", TK_LOOP},
+                {"continue", TK_CONTINUE},
+                {"break", TK_BREAK},
+                {"func", TK_FUNC},
+                {"return", TK_RETURN},
+                {"struct", TK_STRUCT},
+                {"namespace", TK_NAMESPACE},
+                {"global", TK_GLOBAL},
+                {"local", TK_LOCAL},
+                {"interface", TK_INTERFACE},
+                {"class", TK_CLASS},
+                {"static", TK_STATIC},
+                {"super", TK_SUPER},
+                {"public", TK_PUBLIC},
+                {"protected", TK_PROTECTED},
+                {"private", TK_PRIVATE},
+                {"virtual", TK_VIRTUAL},
+                {"override", TK_OVERRIDE},
+                {"is", TK_IS},
+                {"entity", TK_ENTITY},
+                {"component", TK_COMPONENT},
+                {"has", TK_HAS},
+                {"new", TK_NEW},
+                {"del", TK_DEL}
+            });
+            std::ranges::sort(arr, std::less<>{}, &std::pair<std::string_view, Type>::first);
+            return arr;
+        }();
+        auto it = std::lower_bound(
+            std::begin(keywords), std::end(keywords), s,
+            [](const auto& pair, std::string_view val)
+            {
+                return pair.first < val;
+            }
+        );
+        if (it != std::end(keywords) && it->first == s)
+            return it->second;
+        return TK_IDENT;
+    }
+    #define LUNA_TOKEN_TYPE_LIST \
+        /* 注释 */ \
+        LUNA_TOKEN_X(NOTE, "//") \
+        /* 基础 */ \
+        LUNA_TOKEN_X(ASSIGN, "=") \
+        /* 标识符 */ \
+        LUNA_TOKEN_X(IDENT, "<ident>") \
+        /* 字面量 */ \
+        LUNA_TOKEN_X(LIT_INT, "<int>") \
+        LUNA_TOKEN_X(LIT_FLOAT, "<float>") \
+        LUNA_TOKEN_X(LIT_STRING, "<string>") \
+        /* 数学运算 */ \
+        LUNA_TOKEN_X(ADD, "+") \
+        LUNA_TOKEN_X(SUB, "-") \
+        LUNA_TOKEN_X(MUL, "*") \
+        LUNA_TOKEN_X(DIV, "/") \
+        LUNA_TOKEN_X(MOD, "%") \
+        LUNA_TOKEN_X(POW, "**") \
+        LUNA_TOKEN_X(SELF_ADD, "+=") \
+        LUNA_TOKEN_X(SELF_SUB, "-=") \
+        LUNA_TOKEN_X(SELF_MUL, "*=") \
+        LUNA_TOKEN_X(SELF_DIV, "/=") \
+        LUNA_TOKEN_X(SELF_MOD, "%=") \
+        LUNA_TOKEN_X(SELF_POW, "**=") \
+        /* 位运算 */ \
+        LUNA_TOKEN_X(BIT_AND, "&") \
+        LUNA_TOKEN_X(BIT_OR, "|") \
+        LUNA_TOKEN_X(BIT_XOR, "^") \
+        LUNA_TOKEN_X(BIT_XNOR, "`") \
+        LUNA_TOKEN_X(BIT_NOT, "~") \
+        LUNA_TOKEN_X(BIT_SHL, "<<") \
+        LUNA_TOKEN_X(BIT_SHR, ">>") \
+        LUNA_TOKEN_X(SELF_BIT_AND, "&=") \
+        LUNA_TOKEN_X(SELF_BIT_OR, "|=") \
+        LUNA_TOKEN_X(SELF_BIT_XOR, "^=") \
+        LUNA_TOKEN_X(SELF_BIT_XNOR, "`=") \
+        LUNA_TOKEN_X(SELF_BIT_NOT, "~=") \
+        LUNA_TOKEN_X(SELF_BIT_SHL, "<<=") \
+        LUNA_TOKEN_X(SELF_BIT_SHR, ">>=") \
+        /* 比较运算 */ \
+        LUNA_TOKEN_X(EQ, "==") \
+        LUNA_TOKEN_X(NE, "!=") \
+        LUNA_TOKEN_X(LT, "<") \
+        LUNA_TOKEN_X(LE, "<=") \
+        LUNA_TOKEN_X(GT, ">") \
+        LUNA_TOKEN_X(GE, ">=") \
+        /* 逻辑运算 */ \
+        LUNA_TOKEN_X(AND, "&&") \
+        LUNA_TOKEN_X(OR, "||") \
+        LUNA_TOKEN_X(NOT, "!") \
+        /* 保留词 */ \
+        LUNA_TOKEN_X(NIL, "nil") \
+        LUNA_TOKEN_X(TRUE, "true") \
+        LUNA_TOKEN_X(FALSE, "false") \
+        LUNA_TOKEN_X(DEF, "def") \
+        LUNA_TOKEN_X(VAR, "var") \
+        LUNA_TOKEN_X(LET, "let") \
+        LUNA_TOKEN_X(IMPORT, "import") \
+        LUNA_TOKEN_X(IF, "if") \
+        LUNA_TOKEN_X(ELIF, "elif") \
+        LUNA_TOKEN_X(ELSE, "else") \
+        LUNA_TOKEN_X(LOOP, "loop") \
+        LUNA_TOKEN_X(CONTINUE, "continue") \
+        LUNA_TOKEN_X(BREAK, "break") \
+        LUNA_TOKEN_X(FUNC, "func") \
+        LUNA_TOKEN_X(RETURN, "return") \
+        LUNA_TOKEN_X(STRUCT, "struct") \
+        LUNA_TOKEN_X(NAMESPACE, "namespace") \
+        LUNA_TOKEN_X(GLOBAL, "global") \
+        LUNA_TOKEN_X(LOCAL, "local") \
+        LUNA_TOKEN_X(INTERFACE, "interface") \
+        LUNA_TOKEN_X(CLASS, "class") \
+        LUNA_TOKEN_X(STATIC, "static") \
+        LUNA_TOKEN_X(SUPER, "super") \
+        LUNA_TOKEN_X(PUBLIC, "public") \
+        LUNA_TOKEN_X(PROTECTED, "protected") \
+        LUNA_TOKEN_X(PRIVATE, "private") \
+        LUNA_TOKEN_X(VIRTUAL, "virual") \
+        LUNA_TOKEN_X(OVERRIDE, "override") \
+        LUNA_TOKEN_X(IS, "is")\
+        LUNA_TOKEN_X(ENTITY, "entity") \
+        LUNA_TOKEN_X(COMPONENT, "component") \
+        LUNA_TOKEN_X(HAS, "has") \
+        LUNA_TOKEN_X(NEW, "new") \
+        LUNA_TOKEN_X(DEL, "del") \
+        /* 区域 */ \
+        LUNA_TOKEN_X(LPAREN, "(") \
+        LUNA_TOKEN_X(RPAREN, ")") \
+        LUNA_TOKEN_X(LBRACKET, "[") \
+        LUNA_TOKEN_X(RBRACKET, "]") \
+        LUNA_TOKEN_X(LBRACE, "{") \
+        LUNA_TOKEN_X(RBRACE, "}") \
+        /* 其他 */ \
+        LUNA_TOKEN_X(DOT, ".") \
+        LUNA_TOKEN_X(COMMA, ",") \
+        LUNA_TOKEN_X(SEMICOLON, ";") \
+        LUNA_TOKEN_X(COLON, ":") \
+        LUNA_TOKEN_X(DCOLON, "::") \
+        LUNA_TOKEN_X(RARROW, "->") \
+        LUNA_TOKEN_X(LARROW, "<-") \
+        /* 结束 */ \
+        LUNA_TOKEN_X(EOF, "<eof>")
+    static std::string _toSymbol(Type type)
+    {
+        switch (type)
+        {
             #define LUNA_TOKEN_X(name, symbol) case TK_##name: return symbol;
             LUNA_TOKEN_TYPE_LIST
             #undef LUNA_TOKEN_X
@@ -12,7 +175,7 @@ namespace luna::Token
                 return "<?>";
         }
     }
-    std::string to_string(Type type)
+    static std::string _toString(Type type)
     {
         switch (type)
         {
@@ -23,16 +186,15 @@ namespace luna::Token
                 return "UNKNOWN";
         }
     }
-
     std::string Unit::toString() const
     {
-        std::string res = to_string(type) + " " + to_symbol(type);
+        std::string res = _toString(type) + " " + _toSymbol(type);
         if (type == TK_LIT_INT || type == TK_LIT_FLOAT || type == TK_LIT_STRING)
             res += " " + Value::toString(literal);
         return res;
     }
-    std::string Unit::symbol() const
+    std::string Unit::toSymbol() const
     {
-        return to_symbol(type);
+        return _toSymbol(type);
     }
 }
--- src/core/token.hpp
+++ src/core/token.hpp
@@ -5,105 +5,111 @@
 
 namespace luna::Token
 {
-    #define LUNA_TOKEN_TYPE_LIST \
-        /* 注释 */ \
-        LUNA_TOKEN_X(NOTE, "//") \
-        /* 基础 */ \
-        LUNA_TOKEN_X(DEF, "#") \
-        LUNA_TOKEN_X(ASSIGN, "=") \
-        /* 标识符 */ \
-        LUNA_TOKEN_X(IDENT, "<ident>") \
-        /* 字面量 */ \
-        LUNA_TOKEN_X(LIT_INT, "<int>") \
-        LUNA_TOKEN_X(LIT_FLOAT, "<float>") \
-        LUNA_TOKEN_X(LIT_STRING, "<string>") \
-        /* 数学运算 */ \
-        LUNA_TOKEN_X(ADD, "+") \
-        LUNA_TOKEN_X(SUB, "-") \
-        LUNA_TOKEN_X(MUL, "*") \
-        LUNA_TOKEN_X(DIV, "/") \
-        LUNA_TOKEN_X(MOD, "%") \
-        LUNA_TOKEN_X(POW, "**") \
-        LUNA_TOKEN_X(SELF_ADD, "+=") \
-        LUNA_TOKEN_X(SELF_SUB, "-=") \
-        LUNA_TOKEN_X(SELF_MUL, "*=") \
-        LUNA_TOKEN_X(SELF_DIV, "/=") \
-        LUNA_TOKEN_X(SELF_MOD, "%=") \
-        LUNA_TOKEN_X(SELF_POW, "**=") \
-        /* 位运算 */ \
-        LUNA_TOKEN_X(BIT_AND, "&") \
-        LUNA_TOKEN_X(BIT_OR, "|") \
-        LUNA_TOKEN_X(BIT_XOR, "^") \
-        LUNA_TOKEN_X(BIT_XNOR, "`") \
-        LUNA_TOKEN_X(BIT_NOT, "~") \
-        LUNA_TOKEN_X(BIT_SHL, "<<") \
-        LUNA_TOKEN_X(BIT_SHR, ">>") \
-        LUNA_TOKEN_X(SELF_BIT_AND, "&=") \
-        LUNA_TOKEN_X(SELF_BIT_OR, "|=") \
-        LUNA_TOKEN_X(SELF_BIT_XOR, "^=") \
-        LUNA_TOKEN_X(SELF_BIT_XNOR, "`=") \
-        LUNA_TOKEN_X(SELF_BIT_NOT, "~=") \
-        LUNA_TOKEN_X(SELF_BIT_SHL, "<<=") \
-        LUNA_TOKEN_X(SELF_BIT_SHR, ">>=") \
-        /* 比较运算 */ \
-        LUNA_TOKEN_X(EQ, "==") \
-        LUNA_TOKEN_X(NE, "!=") \
-        LUNA_TOKEN_X(LT, "<") \
-        LUNA_TOKEN_X(LE, "<=") \
-        LUNA_TOKEN_X(GT, ">") \
-        LUNA_TOKEN_X(GE, ">=") \
-        /* 逻辑运算 */ \
-        LUNA_TOKEN_X(NOT, "!") \
-        LUNA_TOKEN_X(AND, "&&") \
-        LUNA_TOKEN_X(OR, "||") \
-        /* 关键字 */ \
-        LUNA_TOKEN_X(IMPORT, "import") \
-        LUNA_TOKEN_X(NIL, "nil") \
-        LUNA_TOKEN_X(TRUE, "true") \
-        LUNA_TOKEN_X(FALSE, "false") \
-        LUNA_TOKEN_X(IF, "if") \
-        LUNA_TOKEN_X(ELIF, "elif") \
-        LUNA_TOKEN_X(ELSE, "else") \
-        LUNA_TOKEN_X(LOOP, "loop") \
-        LUNA_TOKEN_X(CONTINUE, "continue") \
-        LUNA_TOKEN_X(BREAK, "break") \
-        LUNA_TOKEN_X(FUNC, "func") \
-        LUNA_TOKEN_X(RETURN, "return") \
-        LUNA_TOKEN_X(STRUCT, "struct") \
-        LUNA_TOKEN_X(INTERFACE, "interface") \
-        LUNA_TOKEN_X(CLASS, "class") \
-        LUNA_TOKEN_X(PUBLIC, "public") \
-        LUNA_TOKEN_X(PRIVATE, "private") \
-        LUNA_TOKEN_X(ENTITY, "entity") \
-        LUNA_TOKEN_X(COMPONENT, "component") \
-        LUNA_TOKEN_X(NAMESPACE, "namespace") \
-        /* 区域 */ \
-        LUNA_TOKEN_X(LPAREN, "(") \
-        LUNA_TOKEN_X(RPAREN, ")") \
-        LUNA_TOKEN_X(LBRACKET, "[") \
-        LUNA_TOKEN_X(RBRACKET, "]") \
-        LUNA_TOKEN_X(LBRACE, "{") \
-        LUNA_TOKEN_X(RBRACE, "}") \
-        /* 其他 */ \
-        LUNA_TOKEN_X(RARROW, "->") \
-        LUNA_TOKEN_X(LARROW, "<-") \
-        LUNA_TOKEN_X(RDARROW, "->>") \
-        LUNA_TOKEN_X(LDARROW, "<<-") \
-        LUNA_TOKEN_X(DOT, ".") \
-        LUNA_TOKEN_X(COMMA, ",") \
-        LUNA_TOKEN_X(SEMICOLON, ";") \
-        LUNA_TOKEN_X(DCOLON, "::") \
-        /* 结束 */ \
-        LUNA_TOKEN_X(EOF, "<eof>")
     enum Type : uint8_t
     {
-        #define LUNA_TOKEN_X(name, symbol) TK_##name,
-        LUNA_TOKEN_TYPE_LIST
-        #undef LUNA_TOKEN_X
-        TK_COUNT
+        /* 注释 */
+        TK_NOTE, // //
+        /* 基础 */
+        TK_ASSIGN, // =
+        /* 标识符 */
+        TK_IDENT, // <ident>
+        /* 字面量 */
+        TK_LIT_INT,    // <int>
+        TK_LIT_FLOAT,  // <float>
+        TK_LIT_STRING, // <string>
+        /* 数学运算 */
+        TK_ADD,      // +
+        TK_SUB,      // -
+        TK_MUL,      // *
+        TK_DIV,      // /
+        TK_MOD,      // %
+        TK_POW,      // **
+        TK_SELF_ADD, // +=
+        TK_SELF_SUB, // -=
+        TK_SELF_MUL, // *=
+        TK_SELF_DIV, // /=
+        TK_SELF_MOD, // %=
+        TK_SELF_POW, // **=
+        /* 位运算 */
+        TK_BIT_AND,       // &
+        TK_BIT_OR,        // |
+        TK_BIT_XOR,       // ^
+        TK_BIT_XNOR,      // `
+        TK_BIT_NOT,       // ~
+        TK_BIT_SHL,       // <<
+        TK_BIT_SHR,       // >>
+        TK_SELF_BIT_AND,  // &=
+        TK_SELF_BIT_OR,   // |=
+        TK_SELF_BIT_XOR,  // ^=
+        TK_SELF_BIT_XNOR, // `=
+        TK_SELF_BIT_NOT,  // ~=
+        TK_SELF_BIT_SHL,  // <<=
+        TK_SELF_BIT_SHR,  // >>=
+        /* 比较运算 */
+        TK_EQ, // ==
+        TK_NE, // !=
+        TK_LT, // <
+        TK_LE, // <=
+        TK_GT, // >
+        TK_GE, // >=
+        /* 逻辑运算 */
+        TK_AND, // &&
+        TK_OR,  // ||
+        TK_NOT, // !
+        /* 保留词 */
+        TK_NIL,       // nil
+        TK_TRUE,      // true
+        TK_FALSE,     // false
+        TK_DEF,       // def
+        TK_VAR,       // var
+        TK_LET,       // let
+        TK_IMPORT,    // import
+        TK_IF,        // if
+        TK_ELIF,      // elif
+        TK_ELSE,      // else
+        TK_LOOP,      // loop
+        TK_CONTINUE,  // continue
+        TK_BREAK,     // break
+        TK_FUNC,      // func
+        TK_RETURN,    // return
+        TK_STRUCT,    // struct
+        TK_NAMESPACE, // namespace
+        TK_GLOBAL,    // global
+        TK_LOCAL,     // local
+        TK_INTERFACE, // interface
+        TK_CLASS,     // class
+        TK_STATIC,    // static
+        TK_SUPER,     // super
+        TK_PUBLIC,    // public
+        TK_PROTECTED, // protected
+        TK_PRIVATE,   // private
+        TK_VIRTUAL,   // virtual
+        TK_OVERRIDE,  // override
+        TK_IS,        // is
+        TK_ENTITY,    // entity
+        TK_COMPONENT, // component
+        TK_HAS,       // has
+        TK_NEW,       // new
+        TK_DEL,       // del
+        /* 区域 */
+        TK_LPAREN,   // (
+        TK_RPAREN,   // )
+        TK_LBRACKET, // [
+        TK_RBRACKET, // ]
+        TK_LBRACE,   // {
+        TK_RBRACE,   // }
+        /* 其他 */
+        TK_DOT,       // .
+        TK_COMMA,     // ,
+        TK_SEMICOLON, // ;
+        TK_COLON,     // :
+        TK_DCOLON,    // ::
+        TK_RARROW,    // ->
+        TK_LARROW,    // <-
+        /* 结束 */
+        TK_EOF // <eof>
     };
-    std::string to_symbol(Type type);
-    std::string to_string(Type type);
+    Type toTypeFromKeyword(std::string_view s);
     struct Unit final
     {
     public:
@@ -116,7 +122,7 @@ namespace luna::Token
         Unit(Type t, std::string_view l, Value::Data v, Position p) : type(t), lexeme(l), literal(std::move(v)), pos(p) {}
     public:
         std::string toString() const;
-        std::string symbol() const;
+        std::string toSymbol() const;
     };
     using List = std::vector<Unit>;
 }
