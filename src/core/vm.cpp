e78f4e207e3099a8325f74f15292967ea93b249c
夏空真宇 <412612365@qq.com>
2026-03-03 23:35:39 +0800

    update

--- src/core/vm.cpp
+++ src/core/vm.cpp
@@ -5,16 +5,15 @@
 
 namespace luna
 {
-    VM::VM() : _lexer(std::make_unique<Lexer>()) {}
-    VM::~VM() = default;
     void VM::runSimple(const std::string& source)
     {
         auto s = util::fileToString(source);
     }
     void VM::runFile(const std::string& path)
     {
-        const auto& tokens = _lexer->tokenize(util::fileToString(path), &_error_reporter);
-        for (const auto& token : tokens)
-            std::cout << token.toString() << std::endl;
+        Lexer lexer {util::fileToString(path), &_error_reporter};
+        const auto& tokens = lexer.start();
+        // for (const auto& token : tokens)
+        //     std::cout << token.toString() << std::endl;
     }
 }
