4932eb34afd8627bbe6093365aa3e6dd6d305359
夏空真宇 <412612365@qq.com>
2026-06-04 23:09:24 +0800

    Update token.hpp

--- rei/src/compiler/token.hpp
+++ rei/src/compiler/token.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 namespace rei
 {
@@ -8,25 +9,60 @@ namespace rei
     , ? .   :  ;
         ... ::
     + - * / %
-    &  |  !
-    && ||
+    &  |  ~
+    && || !
     <  >
     << >>
     =  != <= >= :=
     ==
+    += -= *= /= %=
+    &= |= ~=
+    <<= >>=
+    <- ->
+    # @
     */
-    enum class TokenSymbol
+    enum class TokenSymbol : uint8_t
     {
         LeftParen,  LeftBracket,    LeftBrace,
         RightParen, RightBracket,   RightBrace,
         Comma, Question,    Dot,        Colon,      Semicolon,
                             DotDotDot,  ColonColon,
         Plus, Minus, Star, Slash, Modulo,
-        And,    Pipe,       Bang,
-        AndAnd, PipePipe,
+        And,    Pipe,       Wave,
+        AndAnd, PipePipe,   Bang,
         Less,       Greater,
         LessLess,   GreaterGreater,
         Equal,      BangEqual, LessEqual, GreaterEqual, ColonEqual,
-        EqualEqual
+        EqualEqual,
+        PlusEqual, MinusEqual, StarEqual, SlashEqual, ModuloEqual,
+        AndEqual, PipeEqual, WaveEqual,
+        LessLessEqual, GreaterGreaterEqual,
+        LeftArrow, RightArrow,
+        Pound, At,
+
+        Const, Static,
+
+        If, Elif, Else,
+        While, Until, Continue, Break, Do,
+        Switch, Case, Pass, Default,
+        Func, Return,
+        Namespace, External, Internal,
+
+        Enum, Flag,
+        Struct,
+        Class, Public, Protected, Private, Super, This, Is,
+        Abstract, Sealed,
+        Virtual, Override,
+        New, Del,
+        Interface, Trait, Has,
+        Generics, Where,
+        Attribute,
+
+        Nil,
+        True, False,
+        Int, Float,
+        String,
+
+        EOF
     };
 }
