1937a89fbe8666c28b5a1a5b31be28a93201c5b1
夏空真宇 <412612365@qq.com>
2026-06-11 23:12:21 +0800

    Update config.hpp

--- rei/include/rei/config.hpp
+++ rei/include/rei/config.hpp
@@ -2,9 +2,9 @@
 #include <cstdint>
 #define REI_DEBUG_ENABLE 1
 #define REI_COMPUTED_GOTO_OPT 1
-#define REI_BYTECODE_TYPE uint64_t
-#define REI_BYTECODE_MAX (uint64_t)-1
-#define REI_BYTECODE_NULL (uint64_t)-1
-#define REI_BYTECODE_INT int64_t
+#define REI_BYTECODE_TYPE uint8_t
+#define REI_BYTECODE_MAX (uint8_t)-1
+#define REI_BYTECODE_NULL (uint8_t)-1
+#define REI_BYTECODE_INT int8_t
 #define REI_FUNC_ARG_COUNT_MAX 255
 #define REI_FUNC_ONCE_COUNT_MAX 255
