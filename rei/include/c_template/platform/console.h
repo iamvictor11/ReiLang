1e23ceb016c6cf8e86f2f5bd2fafce93da5cfc58
夏空真宇 <412612365@qq.com>
2026-06-12 21:40:32 +0800

    Update console.h

--- rei/include/c_template/platform/console.h
+++ rei/include/c_template/platform/console.h
@@ -1,14 +1,14 @@
 #ifndef C_TEMPLATE_PLATFORM_CONSOLE_H
 #define C_TEMPLATE_PLATFORM_CONSOLE_H
 
-void osSetConsoleOutputCP_UTF8();
-void osGetChar();
+void osSetConsoleOutputCP_UTF8(void);
+void osGetChar(void);
 
 #ifdef C_TEMPLATE_IMPL_FILE
 
 #include "kua.h"
 
-void osSetConsoleOutputCP_UTF8()
+void osSetConsoleOutputCP_UTF8(void)
 {
 #if C_TEMPLATE_PLATFORM_WINDOWS
     SetConsoleOutputCP(CP_UTF8);
@@ -17,7 +17,7 @@ void osSetConsoleOutputCP_UTF8()
 #else
 #endif
 }
-void osGetChar()
+void osGetChar(void)
 {
 #if C_TEMPLATE_PLATFORM_WINDOWS
     _getch();
