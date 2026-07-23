e7538c051d1454c7111a9514c9761644d3b86372
夏空真宇 <412612365@qq.com>
2026-07-23 15:41:48 +0800

    add kua

---
+++ examples/include/kua.hxx
@@ -0,0 +1,11 @@
+#pragma once
+#include "rei/rei.hxx"
+
+static inline void fs_useUTF8()
+{
+    printf("\x1b%%G");
+}
+static inline void fs_waitEnter()
+{
+    std::cin.get();
+}
---
+++ examples/main/01_a.cxx
@@ -0,0 +1,11 @@
+#include "kua.hxx"
+
+int main(int argc, char *argv[])
+{
+    fs_useUTF8();
+
+    
+
+    fs_waitEnter();
+    return 0;
+}
